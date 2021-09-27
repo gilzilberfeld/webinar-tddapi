d374cce984914fb5f1c0fde98e2f6f6cb1144fff
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-09-27 10:35:10 +0300

    s02 :Added API calls

---
+++ C#/TddApi/NavigationAppTests/s02.add_api_calling_code/Distance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NavigationAppTests.s02.add_api_calling_code
+{
+    internal class Distance
+    {
+        internal int inKm()
+        {
+            return -1;
+        }
+    }
+}
---
+++ C#/TddApi/NavigationAppTests/s02.add_api_calling_code/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavigationAppTests.s02.add_api_calling_code
+{
+    class Location
+    {
+        public Location(string city)
+        {
+
+        }
+    }
+}
