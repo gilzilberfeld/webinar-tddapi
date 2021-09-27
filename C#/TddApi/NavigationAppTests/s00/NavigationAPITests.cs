d2e669457067b23e36ee4195e3002fdbdb579617
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-09-27 09:10:36 +0300

    s0 : Setup

---
+++ C#/TddApi/NavigationAppTests/s00/NavigationAPITests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NavigationAppTests
+{
+    [TestClass]
+    public class NavigationAPITests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
