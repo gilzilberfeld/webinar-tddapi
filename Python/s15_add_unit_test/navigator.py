7c56e2d364a84fc330eace531bf329e796368494
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-03-12 11:08:57 +0200

    s15

---
+++ Python/s15_add_unit_test/navigator.py
@@ -0,0 +1,10 @@
+class Navigator:
+    def __init__(self, starting_point, dist_provider):
+        self.dist_provider = dist_provider
+        self.starting_point = starting_point
+
+    def getDistanceFromDestination(self):
+        return self.dist_provider.get_distance(self.starting_point, self.starting_point)
+
+    def setDestination(self, dest):
+        pass
---
+++ Python/s15_add_unit_test/navigator_unit_tests.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import patch
+
+from hamcrest import assert_that, equal_to
+
+from s15_add_unit_test.distance import Distance
+from s15_add_unit_test.location import Location
+from s15_add_unit_test.navigator import Navigator
+
+
+class NavigatorUnitTests(unittest.TestCase):
+    def setUp(self):
+        self.distProvider = patch('s11_mock_adapter.distance_adapter.DistanceAdapter').start()
+
+    def test_when_created_distance_is_always_zero(self):
+        self.set_distance_to(0)
+        nav = Navigator(Location("London"), self.distProvider)
+        distance = nav.getDistanceFromDestination()
+        assert_that(distance.inKm(), equal_to(0))
+
+    def test_distance_is_calculated_in_km(self):
+        """2500 miles = 4000 kms"""
+        self.set_distance_to(2500)
+        startingPoint = Location("New York City")
+        nav = Navigator(startingPoint, self.distProvider)
+        nav.setDestination(Location("Los Angeles"))
+        distance = nav.getDistanceFromDestination()
+        assert_that(distance.inKm(), equal_to(4000))
+
+    def test_provider_called_correctly(self):
+        startingPoint = Location("New York City")
+        dest = Location("Los Angeles")
+
+        nav = Navigator(startingPoint, self.distProvider)
+        nav.setDestination(dest)
+        nav.getDistanceFromDestination()
+        self.distProvider.get_distance.assert_called_with(startingPoint, dest)
+
+    def set_distance_to(self, miles):
+        self.distProvider.get_distance.return_value = Distance(miles)
+
+
+if __name__ == '__main__':
+    unittest.main()
