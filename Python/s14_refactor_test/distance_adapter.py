eeb07b86700524022501564e6e3585c286e01e9e
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-03-12 10:59:58 +0200

    s14

---
+++ Python/s14_refactor_test/distance_adapter.py
@@ -0,0 +1,6 @@
+from s14_refactor_test.distance import Distance
+
+
+class DistanceAdapter:
+    def get_distance(self, start, dest):
+        return Distance(0)
---
+++ Python/s14_refactor_test/navigator_unit_tests.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch
+
+from hamcrest import assert_that, equal_to
+
+from s14_refactor_test.distance import Distance
+from s14_refactor_test.location import Location
+from s14_refactor_test.navigator import Navigator
+
+
+class NavigatorUnitTests(unittest.TestCase):
+    def setUp(self) :
+        self.distProvider = patch('s11_mock_adapter.distance_adapter.DistanceAdapter').start()
+
+    def test_when_created_distance_is_always_zero(self):
+        self.set_distance_to(0)
+        nav = Navigator(Location("London"), self.distProvider)
+        distance = nav.getDistanceFromDestination()
+        assert_that(distance.inKm(), equal_to(0))
+
+    def test_distance_is_calculated_in_km(self):
+        """2500 miles = 4000 kms"""
+        self.set_distance_to(2500)
+        startingPoint = Location("New York City")
+        nav = Navigator(startingPoint, self.distProvider)
+        nav.setDestination(Location("Los Angeles"))
+        distance = nav.getDistanceFromDestination()
+        assert_that(distance.inKm(), equal_to(4000))
+
+    def set_distance_to(self, miles):
+        self.distProvider.get_distance.return_value = Distance(miles)
+
+
+if __name__ == '__main__':
+    unittest.main()
